1e49db0429d5b4f2def8e0e415f93aaf5ad97954
Contextualist <harzjc@gmail.com>
2016-05-06 22:24:14 +0800

    Try to make "AssignHost" a module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import AssignHost
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,22 +9,9 @@ app.config['DEBUG'] = True
 
 
 @app.route('/')
-def hello():
-    import time
-    import datetime
-    import traceback
-    import quip
-    NextWednesday = datetime.datetime.today() + datetime.timedelta(days = 5)
-    NextWednesdayN = NextWednesday.strftime("%m%d")
-    NextWednesdayS = NextWednesday.strftime("%B %d")
-    if NextWednesdayS[-2] == "0":
-      NextWednesdayS = NextWednesdayS[:-2] + NextWednesdayS[-1];
-    ctx = """<p class='line'>Good Morning AHA!<br/>
-    """ % (NextWednesdayS) # &#8203; (or &#x200b;) stands for a place-holder for a blank <p>
-    client = quip.QuipClient(access_token="Wk9EQU1BcDZFS04=|1483091850|CF037JVoITJPnAET8aHWnZwEZACvrIm7jtkRIQCaX3g=")
-    client.new_document(content=ctx, format="html", title=NextWednesdayN, member_ids=["LHEAOAhm7YS"])
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def assign_do():
+    s = AssignHost.do()
+    return 'Done!%s' % (s)
 
 
 @app.errorhandler(404)
