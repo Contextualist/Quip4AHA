68fd52c81c76b1cc7b9571a9c1604b3b975b81cd
Contextualist <harzjc@gmail.com>
2016-06-03 22:39:40 +0800

    Try variable route and error handler

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import traceback
+import logging
+logging.basicConfig(level=logging.INFO)
+
 from NewDoc import NewDoc
 from AssignHost import AssignHost
+from UpdateWeather import UpdateWeather
 
 from flask import Flask
 app = Flask(__name__)
-app.config['DEBUG'] = True
+#app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -14,12 +19,28 @@ def assign():
     AssignAction = AssignHost()
     return AssignAction.do()
 
+@app.route('/b/<ad_hoc_host>')
+def assign_m(ad_hoc_host):
+    AssignAction = AssignHost(Host=ad_hoc_host.split('+'))
+    return AssignAction.do()
+
 @app.route('/newdoc')
 def newdoc():
     NewDocAction = NewDoc()
-    return NewDocAction.do()
+    return NewDocAction.do()
+
+@app.route('/updateweather')
+def updateweather():
+    UpdateAction = UpdateWeather()
+    return UpdateAction.do()
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    tb = traceback.format_exc()
+    logging.error(tb)
+    return tb, e.code if 'code' in dir(e) else 500
