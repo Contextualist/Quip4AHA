8fd10e7b91d8e85fcb7d9ff64d657a0fa952effd
Contextualist <harzjc@gmail.com>
2016-06-09 20:14:03 +0800

    bug fix; enhance no-padding; enhance logic

--- NewDoc.py
+++ NewDoc.py
@@ -1,3 +1,4 @@
+import urllib2
 from quip4aha import week, QuipClient4AHA, InvalidOperation
 
 class NewDoc(object):
@@ -5,26 +6,23 @@ class NewDoc(object):
     def __init__(self):
         NextWednesday = week.RecentWeekDay('next Wednesday')
         self.NextWednesdayN = NextWednesday.strftime("%m%d")
-        self.NextWednesdayS = NextWednesday.strftime("%B %d")
-        if self.NextWednesdayS[-2] == "0":
-            self.NextWednesdayS = self.NextWednesdayS[:-2] + self.NextWednesdayS[-1];
+        self.NextWednesdayS = NextWednesday.strftime("%B %-d")
         self.ctx = ""
         self.client = QuipClient4AHA()
 
     def do(self):
+        template = urllib2.urlopen("http://pastebin.com/raw/3cLgvDXe").read()
+        # In the template, &#8203; (or &#x200b;) stands for a place-holder for a blank <p>.
+        if template == "cancel":
+            raise InvalidOperation("The template indicates a cancelation for this week!")
+        self.ctx = template.format(self.NextWednesdayS)
+        
         try:
             self.client.get_latest_script_ID()
         except InvalidOperation:
             pass
         else:
             raise InvalidOperation("Redundancy Warning: The script has already been created!")
-        
-        template = urllib2.urlopen(
-            "http://pastebin.com/raw/3cLgvDXe").read()
-            # &#8203; (or &#x200b;) stands for a place-holder for a blank <p>
-        if template == "cancel":
-            raise InvalidOperation("The template indicates a cancelation for this week!")
-        self.ctx = template.format(self.NextWednesdayS)
         
         self.client.new_document(content=self.ctx, format="html", title=self.NextWednesdayN, member_ids=[self.client.AHABC_ID])
         return "Done!"
